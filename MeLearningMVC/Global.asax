2ad66194d85a241d3f2649c546c9a09e5a9e7483
jamiestorey <jamie@jamiestorey.com>
2016-05-19 18:54:49 +0100

    Add project files.

---
+++ MeLearningMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeLearningMVC.MvcApplication" Language="C#" %>
---
+++ MeLearningMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeLearningMVC.Startup))]
+namespace MeLearningMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
