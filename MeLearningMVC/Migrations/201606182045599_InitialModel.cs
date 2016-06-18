83d5fda2f08c30bd57700c8eaea6131f300fcca1
jamiestorey <jamie@jamiestorey.com>
2016-06-18 21:53:11 +0100

    Enabled migrations.

---
+++ MeLearningMVC/Migrations/201606182045599_InitialModel.cs
@@ -0,0 +1,16 @@
+namespace MeLearningMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ MeLearningMVC/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace MeLearningMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MeLearningMVC.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "MeLearningMVC.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(MeLearningMVC.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
