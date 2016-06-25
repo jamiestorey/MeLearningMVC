2af560de803fea3072fdba741ac0002c4eecf747
jamiestorey <jamie@jamiestorey.com>
2016-06-25 22:00:42 +0100

    Create Add a Gig form.

---
+++ MeLearningMVC/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using MeLearningMVC.Models;
+using MeLearningMVC.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MeLearningMVC.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
