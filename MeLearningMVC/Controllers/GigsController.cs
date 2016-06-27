c98217912592295ef3676de57fcba8217f332641
jamiestorey <jamie@jamiestorey.com>
2016-06-27 21:46:39 +0100

    Implement Saving a Gig

--- MeLearningMVC/Controllers/GigsController.cs
+++ MeLearningMVC/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using MeLearningMVC.Models;
 using MeLearningMVC.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -14,6 +16,7 @@ namespace MeLearningMVC.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -21,6 +24,25 @@ namespace MeLearningMVC.Controllers
                 Genres = _context.Genres.ToList()
             };
             return View(viewModel);
+
+        }
+
+        [Authorize]
+        [HttpPost]
+        public System.Web.Mvc.ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig()
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
